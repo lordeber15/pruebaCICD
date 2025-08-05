9c9a52ac90e4dee2f9bc0d7b88adc57c21dd357a
lordeber15 <eberpalomino@hotmail.com>
2025-08-04 23:17:47 -0500

    listo

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Prueba de CI CD nuevos cambios nuevos</h1>
+      <h1>Conseguido por fin CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
