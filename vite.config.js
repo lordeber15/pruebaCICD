ba7f978c5df85b49ecdfd4b857e5b4760709874f
lordeber15 <eberpalomino@hotmail.com>
2025-08-04 20:59:24 -0500

    cambios revertidos

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0", // escuche todas las IPs
-    port: 5173,
-    hmr: {
-      host: "157.245.143.86", // IP pública del servidor
-      protocol: "ws", // o 'wss' si usas HTTPS
-      port: 5173,
-    },
-  },
   plugins: [react()],
 });
