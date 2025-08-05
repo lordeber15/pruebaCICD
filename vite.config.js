189dff1fdc40ea7b553ee54bf294fafeedc208ba
lordeber15 <eberpalomino@hotmail.com>
2025-08-04 20:57:37 -0500

    cambios nuevos websoquet

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0", // escuche todas las IPs
+    port: 5173,
+    hmr: {
+      host: "157.245.143.86", // IP pública del servidor
+      protocol: "ws", // o 'wss' si usas HTTPS
+      port: 5173,
+    },
+  },
   plugins: [react()],
-})
+});
